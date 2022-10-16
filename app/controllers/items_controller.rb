5bcad2a7134508c3803c27c9246cfcb30d0236b2
ababab0020 <ababab0020@icloud.com>
2022-10-16 16:25:49 +0900

    コメントアウト

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
-  before_action :set_item, only: [ :edit, :update]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
   #before_action :set_item, only: [:show, :edit, :update, :destroy]
  
    def index
