8c48b9acbc7c286ef9bd3ddbb740c479e479cbc4
ababab0020 <ababab0020@icloud.com>
2022-10-16 20:07:34 +0900

    商品削除機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
-  #before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
  
    def index
      @items = Item.includes(:user).order('created_at DESC')
@@ -37,8 +36,14 @@ class ItemsController < ApplicationController
   def show
   end
  
-   #def destroy
-   #end
+   def destroy
+    if @item.user_id == current_user.id
+      @item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+   end
    
    private
  
